41dd8c0ae29539e84c8d595c2b3cbf4cf277572e
WarrenMfg <warrenmfg@icloud.com>
2022-11-27 18:14:08 -0500

    abstracts init function

--- index.ts
+++ index.ts
@@ -1,49 +1,9 @@
 #!/usr/bin/env node
-import chalk from 'chalk';
-import api from './api/index.js';
-import alert from './utils/alert.js';
-import clear from './utils/clear.js';
-import formatAndPrint from './utils/formatAndPrint.js';
-import formatWord from './utils/formatWord.js';
-import handleUncaughtException from './utils/handleUncaughtException.js';
+import handleUncaughtException from './utils/handleUncaughtException';
+import init from './utils/init';
 
 // handle uncaught exceptions
 handleUncaughtException();
 
-/**
- * Init
- */
-const init = async () => {
-  // clear console
-  clear();
-
-  const arg = process.argv[2];
-
-  if (!arg) {
-    alert.warning('Please enter a word.');
-    return;
-  }
-
-  // format argument
-  const word = formatWord(arg);
-
-  // get definition
-  const definition = await api.getDefinition(word);
-
-  // ensure definition exists
-  if (!definition?.defs?.length) {
-    alert.warning(
-      `Could not find definition for ${chalk.blue.bold(
-        `"${word}"`
-      )}. Please try a different word.`
-    );
-
-    return;
-  }
-
-  const synonyms = await api.getSynonyms(word);
-
-  formatAndPrint(word, definition, synonyms);
-};
-
-init();
+// run program
+init(process.argv[2]);
---
+++ utils/init.ts
@@ -0,0 +1,42 @@
+import chalk from 'chalk';
+import api from '../api';
+import alert from './alert';
+import clear from './clear';
+import formatAndPrint from './formatAndPrint';
+import formatWord from './formatWord';
+
+/**
+ * Init
+ */
+const init = async (arg?: string) => {
+  // clear console
+  clear();
+
+  if (!arg) {
+    alert.warning('Please enter a word.');
+    return;
+  }
+
+  // format argument
+  const word = formatWord(arg);
+
+  // get definition
+  const definition = await api.getDefinition(word);
+
+  // ensure definition exists
+  if (!definition?.defs?.length) {
+    alert.warning(
+      `Could not find definition for ${chalk.blue.bold(
+        `"${word}"`
+      )}. Please try a different word.`
+    );
+
+    return;
+  }
+
+  const synonyms = await api.getSynonyms(word);
+
+  formatAndPrint(word, definition, synonyms);
+};
+
+export default init;
