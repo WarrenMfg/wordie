389581e4c550f1cef651180e2ff613c4a76be2db
WarrenMfg <warrenmfg@icloud.com>
2022-11-28 06:08:14 -0500

    adds clear and node version check

--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
 #!/usr/bin/env node
+import isRunningNodeVersion from './utils/isRunningNodeVersion';
 import handleUncaughtException from './utils/handleUncaughtException';
 import init from './utils/init';
+import clear from './utils/clear';
 
 // handle uncaught exceptions
 handleUncaughtException();
 
-// run program
-init(process.argv[2]);
+// clear console
+clear();
+
+// check node version
+if (isRunningNodeVersion(14)) {
+  // run program
+  init(process.argv[2]);
+}
